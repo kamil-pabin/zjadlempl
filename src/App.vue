b082f0f06a38ed2a0ea7f3ba192d69841e2c95fb
Kamil Pabin <kpabin9@gmail.com>
2021-11-03 20:29:42 +0100

    Update and fixes

--- src/App.vue
+++ src/App.vue
@@ -41,20 +41,34 @@
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;500&family=Raleway:wght@200;300;500&display=swap');
 @import '../node_modules/@braid/vue-formulate/themes/snow/snow.scss';
 .invert { filter: invert(100%); }
+html {
+  //background-image: url("./assets/tlo.png");
+  background: url("./assets/tlo.png") no-repeat center fixed;
+  background-size: cover;
+}
+body{
+  background:transparent;
+}
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   font-family: 'Raleway', sans-serif;
   position:absolute;
+  width:100%;
+  height:100%;
+  padding:0;
+  margin:0;
+  border: 0 !important;
+  background:transparent;
 }
 .tabButton {
-    transition: .5s ease;
-    text-decoration: 0;
-    text-transform: uppercase;
-    font-size: 0.8rem;
-    font-weight: 500;
-  }
+  transition: .5s ease;
+  text-decoration: 0;
+  text-transform: uppercase;
+  font-size: 0.8rem;
+  font-weight: 500;
+}
 #sidebar-backdrop {
 
 }
@@ -111,8 +125,32 @@
   padding-bottom: 0;
   margin-bottom:0;
   display:flex;
-  max-height:100%;
+  margin:0;
+  padding:0;
+  height:100vh;
+  width:100vw;
   background:transparent;
+  padding:0;
+  margin:0;
+}
+.grupaZdjec{
+  width: auto;
+  max-width: 100%;
+  margin-top:-100px;
+  padding:0;
+  height:100%;
+  display:flex;
+  overflow: hidden;
+}
+.grupaZdjec .zdjecie{
+  display:flex;
+  margin:0;
+  padding:0;
+}
+.grupaZdjec .zdjecie img{
+  max-width:100%;
+  height:100%;
+  object-fit: cover;
 }
 
 @media only screen and (max-width: 1100px) {
@@ -133,5 +171,15 @@
 .card-body.text-center.interior {
     padding: 0;
     height:100%;
+}
+
+.card{
+  background-color:transparent !important;
+  background:transparent !important;
+  border: 0 !important;
+}
+#card{
+  background-color:transparent !important;
+  background:transparent !important;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,17 +12,6 @@
         </div>
       </div>
     </div>
-    <div class="grupaZdjec">
-      <div class="zdjecie" id="sushi">
-        <img src="../assets/sushi.png" alt="Responsive image">
-      </div>
-      <div class="zdjecie" id="pizza">
-        <img src="../assets/pizza.png" alt="Responsive image">
-      </div>
-      <div class="zdjecie" id="stek">
-        <img src="../assets/stek.png" alt="Responsive image">
-      </div>
-    </div>
   </div>
 </template>
 
@@ -52,51 +41,39 @@ export default {
 .home{
   text-align: center;
   width:100%;
-  height:100;
+  height:100%;
   color: white;
   padding:0;
-  margin:0;
-  max-height:100%;
+  margin-left:0px;
 }
 #poleWyszukiwania{
-  width:50vw;
   margin:auto;
-  display:flex;
+  max-width:20rem;
+  height:1rem;
+  display:flexbox;
 }
 #buton{
-  margin-left:10px;
+  margin-top: 2%;
+}
+#buton a{
+  padding:0;
+  margin:0;
 }
 #tlo{
-  border-bottom-right-radius: 60% 25%;
-  border-bottom-left-radius: 70% 25%;
   background: #0f4ec2;
+  //background-image: url("../assets/pizza.png");
+  //background-image: url("../assets/sushi.png");
+  //background-image: url("../assets/stek.png");
   margin-bottom: 0px;
   padding: 20px;
   width: 100%;
   height: 40vh;
-}
-.grupaZdjec{
-  width: auto;
-  max-width: 100%;
-  margin-top:-100px;
-  padding:0;
-  height:100%;
-  display:flex;
-  overflow: hidden;
-}
-.grupaZdjec .zdjecie{
-  display:flex;
-  margin:0;
-  padding:0;
-}
-.grupaZdjec .zdjecie img{
-  max-width:100%;
-  height:100%;
-  object-fit: cover;
+  border-bottom-right-radius: 60% 25%;
+  border-bottom-left-radius: 60% 25%;
 }
 h1{
   overflow: hidden;
-  font-size:4vw;
+  font-size:1.7rem;
   font-weight: 300;
   margin: 0;
   margin-top:7vh;
@@ -110,13 +87,60 @@ h1{
 }
 @media screen and (min-width: 1200px) {
   h1 {
-     font-size: 3rem;
+     font-size: 2.3rem;
      margin-top:7vh;
   }
   #poleWyszukiwania{
-  width:40rem;
-  margin:auto;
-  display:flex;
+    margin:auto;
+    max-width:30rem;
+    height:2.5rem;
+    display:flex;
+  }
+  #buton{
+    margin-top: 0%;
+    margin-left:10px;
+  }
+  #tlo{
+    border-bottom-right-radius: 60% 25%;
+    border-bottom-left-radius: 60% 25%;
+    background: #0f4ec2;
+    //background-image: url("../assets/pizza.png"), url("../assets/sushi.png"), url("../assets/stek.png");
+    //background-position: right, between, left;
+    //background-repeat: no-repeat, no-repeat, no-repeat;
+    //background-image: url("../assets/sushi.png");
+    //background-image: url("../assets/stek.png");
+    margin-bottom: 0px;
+    padding: 20px;
+    width: 100%;
+    height: 40vh;
+  }
 }
+@media screen and (max-width: 600px) {
+  h1 {
+     font-size: 1.3rem;
+     margin-top:7vh;
+  }
+  #poleWyszukiwania{
+    max-width:20rem;
+    margin:auto;
+    height:2rem;
+    display:flexbox;
+  }
+  #buton{
+    margin-top: 2%;
+    margin-left:0px;
+  }
+  #tlo{
+    border-bottom-right-radius: 60% 25%;
+    border-bottom-left-radius: 60% 25%;
+    background: #0f4ec2;
+    //background-image: url("../assets/pizza.png");
+    //background-image: url("../assets/sushi.png");
+    //background-image: url("../assets/stek.png");
+    margin-bottom: 0px;
+    padding: 20px;
+    width: 100%;
+    height: 40vh;
+  }
 }
 </style>
