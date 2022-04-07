360e14d86e4d270d5fbffd65ac071628cface43e
Kamil Pabin <kpabin9@gmail.com>
2022-04-07 20:16:51 +0200

    deploy

---
+++ dist/js/chunk-4dffa580.6f48db8c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-4dffa580"],{"61fa":function(t,n,e){"use strict";e("cf5a")},cf5a:function(t,n,e){},f820:function(t,n,e){"use strict";e.r(n);var a=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"about"})},c=[],u={name:"About",metaInfo:{title:"Zjadłem.pl | Dowiedz się więcej!"},data:function(){return{value:"",context:null}}},i=u,f=(e("61fa"),e("2877")),o=Object(f["a"])(i,a,c,!1,null,"551682b5",null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-4dffa580.6f48db8c.js.map
